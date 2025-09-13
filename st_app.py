ddcd0c2b6cc1e1d532b0aa352b3cf4f666fe3112
Andreas Dutzler <a.dutzler@gmail.com>
2025-09-13 22:35:08 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -1,163 +1,32 @@
-import logging
 import streamlit as st
-from textwrap import wrap
-
 from stpyvista import stpyvista
 from stpyvista.utils import start_xvfb
-import pantry.stpyvista_pantry as stpv
-from pantry.webapp_fragments import (
-    gallery,
-    fill_up_main_window,
-    fill_install_instructions,
-)
-
-# Hide param warnings
-logging.getLogger("param.main").setLevel(logging.CRITICAL)
+import felupe as fem
 
 # Initial configuration
 start_xvfb()
-print(f"--> IP: {st.context.ip_address or 'Not-found'}")
 
+st.title("A Streamlit app for FElupe")
+n = st.slider("Number of points per axis", 2, 11, 4)
+v = st.slider("Stretch", 1.0, 2.0, 2.0)
 
-def main():
-    st.set_page_config(
-        page_title="stpyvista · Show 3D visualizations from PyVista in Streamlit",
-        page_icon="🧊",
-        layout="centered",
-        initial_sidebar_state="auto",
-    )
+progress_bar = st.progress(0)
+def show_progress(i, j, substep):
+    progress_bar.progress((1 + j) / len(move))
 
-    # Add styling with CSS selectors
-    with open("assets/style.css") as f:
-        st.html(f"""<style>{f.read()}</style>""")
+mesh = fem.Cube(n=n)
+region = fem.RegionHexahedron(mesh)
+field = fem.FieldContainer([fem.Field(region, dim=3)])
 
-    if st.context.is_embedded:
-        with open("assets/style_embed.css") as f:
-            st.html(f"""<style>{f.read()}</style>""")
+boundaries, loadcase = fem.dof.uniaxial(field, clamped=True)
+umat = fem.NeoHooke(mu=1, bulk=2)
+solid = fem.SolidBody(umat=umat, field=field)
+move = fem.math.linsteps([0, v - 1], num=5)
 
-    # --------------------------------------------
-    # Full version
-    # --------------------------------------------
-    if not st.context.is_embedded:
-        # General layout
-        with st.sidebar:
-            side_title_container = st.empty()
-            "****"
-            side_gallery_container = st.container()
-            "****"
-            side_other_container = st.empty()
+ramp = {boundaries["move"]: move}
+step = fem.Step(items=[solid], ramp=ramp, boundaries=boundaries)
+job = fem.Job(steps=[step], callback=show_progress)
+job.evaluate(tol=1e-2)
 
-        main_container = st.empty()
-
-        # Figure out section
-        selection_from_query = st.query_params.get("gallery", None)
-
-        if selection_from_query in gallery.keys():
-            st.session_state["gallery_select"] = selection_from_query
-
-        with side_gallery_container:
-            st.subheader("Gallery", anchor=False)
-
-            selection = st.pills(
-                "Gallery selection",
-                gallery.keys(),
-                selection_mode="single",
-                default=None,
-                format_func=lambda x: "\n\n".join(wrap(gallery[x].__doc__, 12)),
-                label_visibility="collapsed",
-                on_change=st.query_params.clear,
-                key="gallery_select",
-            )
-
-            selection = selection_from_query or selection
-
-        with side_other_container.container():
-            # Add badges to sidebar
-            with open("assets/badges.html") as f:
-                st.subheader("Useful links", anchor=False)
-                st.html(f"""{f.read()}""")
-
-        if not selection:
-            with side_title_container.container():
-                st.title("[🧊](https://github.com/edsaac/stpyvista)", anchor=False)
-                st.caption(
-                    """
-                    [`stpyvista`](https://github.com/edsaac/stpyvista) displays PyVista 
-                    plotter objects in streamlit web apps using 
-                    [`panel`](https://panel.holoviz.org/reference/panes/VTK.html) and
-                    [`trame`](https://github.com/Kitware/trame)
-                    """
-                )
-
-            with main_container.container():
-                st.header("`stpyvista`", anchor="stpyvista")
-                st.subheader("Show PyVista 3D visualizations in Streamlit", anchor=False)
-
-                ## Send plotter to streamlit
-                plotter = stpv.intro()
-                stpyvista(
-                    plotter,
-                    panel_kwargs=dict(
-                        orientation_widget=True,
-                        interactive_orientation_widget=True,
-                    ),
-                    bokeh_resources="CDN",
-                )
-
-                st.info("Check the examples gallery in the sidebar!", icon="👈")
-
-                fill_install_instructions()
-                fill_up_main_window()
-
-        # *************************************
-
-        else:
-            with side_title_container.container():
-                st.header(
-                    """[🧊 `stpyvista`](https://stpyvista.streamlit.app)""",
-                    anchor=False,
-                )
-
-            main_container.empty()
-            st.query_params["gallery"] = selection
-
-            with main_container.container():
-                gallery[selection]()
-
-    # --------------------------------------------
-    # Embeded version
-    # --------------------------------------------
-    else:
-        main_container = st.empty()
-
-        with main_container.container():
-            plotter = stpv.intro()
-            stpyvista(
-                plotter,
-                panel_kwargs=dict(
-                    orientation_widget=True,
-                    interactive_orientation_widget=True,
-                ),
-            )
-
-            st.subheader(
-                "[🧊 `stpyvista`](https://stpyvista.streamlit.app)",
-                anchor=False,
-            )
-
-            st.subheader(
-                ":green[Show PyVista 3D visualizations in Streamlit]",
-                anchor=False,
-            )
-
-            st.subheader(
-                "[![Explore the gallery!](https://img.shields.io/badge/Community%20Cloud-Explore%20the%20gallery!-informational?style=flat&logo=streamlit&logoColor=red&color=pink)](https://stpyvista.streamlit.app)",
-                anchor="Launch",
-            )
-
-            fill_install_instructions()
-            fill_up_main_window()
-
-
-if __name__ == "__main__":
-    main()
+plotter = solid.plot("Principal Values of Cauchy Stress")
+stpyvista(plotter)
