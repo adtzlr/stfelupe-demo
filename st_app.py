252d0e90c4d3a0cee945bec1bb8c8409c12e9515
Andreas Dutzler <a.dutzler@gmail.com>
2025-09-13 22:38:46 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -8,29 +8,29 @@ start_xvfb()
 
 import pyvista as pv
 
-plotter = pv.Plotter()
+# plotter = pv.Plotter()
 
-# st.title("A Streamlit app for FElupe")
-# n = st.slider("Number of points per axis", 2, 11, 4)
-# v = st.slider("Stretch", 1.0, 2.0, 2.0)
+st.title("A Streamlit app for FElupe")
+n = st.slider("Number of points per axis", 2, 11, 4)
+v = st.slider("Stretch", 1.0, 2.0, 2.0)
 
-# progress_bar = st.progress(0)
-# def show_progress(i, j, substep):
-#     progress_bar.progress((1 + j) / len(move))
+progress_bar = st.progress(0)
+def show_progress(i, j, substep):
+    progress_bar.progress((1 + j) / len(move))
 
-# mesh = fem.Cube(n=n)
-# region = fem.RegionHexahedron(mesh)
-# field = fem.FieldContainer([fem.Field(region, dim=3)])
+mesh = fem.Cube(n=n)
+region = fem.RegionHexahedron(mesh)
+field = fem.FieldContainer([fem.Field(region, dim=3)])
 
-# boundaries, loadcase = fem.dof.uniaxial(field, clamped=True)
-# umat = fem.NeoHooke(mu=1, bulk=2)
-# solid = fem.SolidBody(umat=umat, field=field)
-# move = fem.math.linsteps([0, v - 1], num=5)
+boundaries, loadcase = fem.dof.uniaxial(field, clamped=True)
+umat = fem.NeoHooke(mu=1, bulk=2)
+solid = fem.SolidBody(umat=umat, field=field)
+move = fem.math.linsteps([0, v - 1], num=5)
 
-# ramp = {boundaries["move"]: move}
-# step = fem.Step(items=[solid], ramp=ramp, boundaries=boundaries)
-# job = fem.Job(steps=[step], callback=show_progress)
-# job.evaluate(tol=1e-2)
+ramp = {boundaries["move"]: move}
+step = fem.Step(items=[solid], ramp=ramp, boundaries=boundaries)
+job = fem.Job(steps=[step], callback=show_progress)
+job.evaluate(tol=1e-2)
 
 # plotter = solid.plot("Principal Values of Cauchy Stress")
 # stpyvista(plotter)
